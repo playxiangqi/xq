faa5af9c86b1da469fa5aaec3368c1c56b41858b
Andrew Hu <andrew.hu@pithecine.com>
2021-01-31 08:47:30 -0500

    board svg

--- xq_app/src/Board.svelte
+++ xq_app/src/Board.svelte
@@ -2,4 +2,13 @@
   import BoardSVG from '../assets/svg/xiangqi-board.min.svg';
 </script>
 
-<BoardSVG />
+<div class="board-container">
+  <BoardSVG />
+</div>
+
+<style lang="scss">
+  .board-container :global(svg) {
+    background-color: #fcaf3e;
+    border: solid 2px black;
+  }
+</style>
