78e51436acf43269cb993664425b0ebc021706b7
Andrew Hu <andrew.hu@pithecine.com>
2021-02-02 01:27:16 -0500

    remove comments

--- xq_app/src/components/board/store.ts
+++ xq_app/src/components/board/store.ts
@@ -41,27 +41,3 @@ export function createBoardState(dimensions: Dimensions) {
     },
   };
 }
-
-// export function movePiece(
-//   index: number,
-//   point: readonly [number, number],
-//   coords?: [number, number]
-// ) {
-//   console.log('we here boyz');
-//   console.log(point);
-//   console.log(coords);
-//   store.update((state) => {
-//     const [rank, file] = point;
-//     state.layout[index].rank = rank;
-//     state.layout[index].file = file;
-//     state.layout[index].position = coords;
-//     return state;
-//   });
-// }
-
-// export function dropPiece(index: number) {
-//   store.update((state) => {
-//     state.layout[index].grabbing = false;
-//     return state;
-//   });
-// }
