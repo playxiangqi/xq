f2550f6dc1dd0cae16611d8f596a0e1b49ca0033
Andrew Hu <andrew.hu@pithecine.com>
2021-02-02 20:22:27 -0500

    update: 🐛✨ Phoenix channels + AuthChannel module naming fix

---
+++ xq_app/src/utils/channels.ts
@@ -0,0 +1,33 @@
+import { Socket } from 'phoenix';
+
+let socket = new Socket('/socket');
+socket.connect();
+
+export type PhoenixEvent = (
+  event: string,
+  payload: Record<string, any>
+) => void;
+
+export function createChannel(topic: string, onMessage: PhoenixEvent) {
+  const channel = socket.channel(topic, { client: 'browser' });
+
+  channel.onMessage = (event, payload) => {
+    if (event != null && payload != null) {
+      onMessage(event, payload);
+    }
+
+    return payload;
+  };
+
+  channel
+    .join()
+    .receive('ok', ({ messages }) => {
+      console.log(`successfully joined channel ${topic}, ${messages}`);
+    })
+    .receive('error', ({ reason }) => {
+      console.error(`failed to join channel ${topic}, ${reason}`);
+    });
+
+  return (event: string, payload: Record<string, any>) =>
+    channel.push(event, payload);
+}
