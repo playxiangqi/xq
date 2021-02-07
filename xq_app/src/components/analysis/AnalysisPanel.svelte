b812debc86d618460bdc21e5d52eaf3bbe44c1a9
Andrew Hu <andrew.n.hu.87@gmail.com>
2021-02-07 16:12:05 -0500

    feat: ✨🎨 Load historical game + board state from API

--- xq_app/src/components/analysis/AnalysisPanel.svelte
+++ xq_app/src/components/analysis/AnalysisPanel.svelte
@@ -1,18 +1,18 @@
 <script lang="ts">
-  import { createBoardState, notationToMove, toHanzi } from 'components/board';
+  import {
+    createBoardState,
+    fetchBoardState,
+    notationToMove,
+    toHanzi,
+  } from 'components/board';
   import type { Dimensions, Move } from 'components/board';
-  import { createAuthStore } from 'services/auth/store';
-
-  const { store: authStore, broadcast: sendAuthRequest } = createAuthStore();
-  // TODO: moves history in boardstate
 
   export let dimensions: Dimensions;
   export let boardState: ReturnType<typeof createBoardState>;
 
   const { store, slidePiece } = boardState;
 
-  $: moveEndIndex = $store.moves.length;
-
+  let promise = fetchBoardState();
   let currentTurnIndex = -1;
 
   type Notation = 'algebraic';
@@ -60,9 +60,13 @@
     currentTurnIndex = Math.max(-1, currentTurnIndex - 1);
   }
 
-  function nextMove() {
+  async function nextMove() {
     currentTurnIndex = Math.min(58, currentTurnIndex + 1);
-    const move = notationToMove(historicalGame.moves[currentTurnIndex]);
+    const {
+      game: { moves },
+    } = await promise;
+
+    const move = notationToMove(moves[currentTurnIndex]);
     slidePiece(move);
     playSound();
   }
@@ -70,84 +74,6 @@
   function skipToEnd() {
     currentTurnIndex = 58;
   }
-
-  // Hard-coded snippet, to be pulled from database with normalized key-values
-  const historicalGame = {
-    'Play Count': '59',
-    Result: 'Red DRAW',
-    Opening: 'Adviser opening',
-    'Last Modify': '2011/10/30 12:52:26',
-    'game index': '34561',
-    Hits: '5385',
-    Event: '2011 China Individual Xiangqi Championship Men',
-    Date: '2011-10-25',
-    Round: 'Round 11',
-    'Red Club': 'HuBei',
-    'Red player': 'Li XueSong',
-    'Black Club': 'BeiJing',
-    'Black player': 'Sun Bo',
-    moves: [
-      'A4+5',
-      'p7+1',
-      'C2=3',
-      'b7+5',
-      'N2+1',
-      'n8+7',
-      'R1=2',
-      'r9=8',
-      'C8=4',
-      'n2+1',
-      'N8+7',
-      'r1=2',
-      'R9=8',
-      'c8+4',
-      'R8+4',
-      'c2=3',
-      'R8=4',
-      'r2+1',
-      'B7+5',
-      'r2=8',
-      'P7+1',
-      'c8+1',
-      'P1+1',
-      'p1+1',
-      'P3+1',
-      'c8=6',
-      'R2+8',
-      'r8+1',
-      'A5+4',
-      'r8+3',
-      'N7+6',
-      'c3=2',
-      'A6+5',
-      'c2+3',
-      'P3+1',
-      'r8=7',
-      'B5+3',
-      'r7=4',
-      'C3+5',
-      'r4+1',
-      'R4=6',
-      'c2=4',
-      '+B-5',
-      'n1+2',
-      'C3-4',
-      'p1+1',
-      'C3=1',
-      'n2+1',
-      'C1=9',
-      'p1+1',
-      'N1+2',
-      'c4=9',
-      'N2+1',
-      'p1=2',
-      'P5+1',
-      'p2=3',
-      'N1+3',
-      '+p=4',
-      'N3-5',
-    ],
-  };
 
   // Sound Effects
   const audio = new Audio('./sounds/drop-piece.wav');
@@ -162,30 +88,39 @@
 <div class="panel analysis-panel">
   <!-- <p>Joined lobby as: {$authStore.username}</p> -->
   <p class="panel-heading">Database Explorer</p>
-  <div class="game-info">
-    <div class="players">
-      {historicalGame['Red player']} vs. {historicalGame['Black player']}
-    </div>
-    <div class="venue">
-      {historicalGame.Event}, {historicalGame.Date}
-    </div>
-    <div class="opening-name">{historicalGame.Opening}</div>
-  </div>
-  <div class="moves-container">
-    <!-- {#each moves.slice(0, moveEndIndex) as move, i} -->
-    {#each generateMoveNotationClassical(historicalGame.moves) as { moveNum, moveRed, moveBlack }, i}
-      <div class="panel-block move">
-        <!-- {generateMoveNotation(move, i)} -->
-        <span class="move-num">{moveNum}.</span>
-        <span class="move-red" class:current={currentTurnIndex === i * 2}
-          >{moveRed}</span
-        >
-        <span class="move-black" class:current={currentTurnIndex === i * 2 + 1}
-          >{moveBlack}</span
-        >
+  {#await promise}
+    <div class="game-info loading">Loading Game...</div>
+    <div class="moves-container loading" />
+  {:then { game }}
+    <div class="game-info">
+      <div class="players">
+        {game.redPlayer} vs. {game.blackPlayer}
       </div>
-    {/each}
-  </div>
+      <div class="venue">
+        {game.event}
+      </div>
+      <div class="date">
+        {new Date(game.date).toDateString()}
+      </div>
+      <div class="opening-name">{game.data.opening}</div>
+    </div>
+    <div class="moves-container">
+      <!-- {#each moves.slice(0, moveEndIndex) as move, i} -->
+      {#each generateMoveNotationClassical(game.moves) as { moveNum, moveRed, moveBlack }, i}
+        <div class="panel-block move">
+          <!-- {generateMoveNotation(move, i)} -->
+          <span class="move-num">{moveNum}.</span>
+          <span class="move-red" class:current={currentTurnIndex === i * 2}
+            >{moveRed}</span
+          >
+          <span
+            class="move-black"
+            class:current={currentTurnIndex === i * 2 + 1}>{moveBlack}</span
+          >
+        </div>
+      {/each}
+    </div>
+  {/await}
   <div class="panel-block move-buttons">
     <button
       class="button"
