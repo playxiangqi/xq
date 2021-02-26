e45997ef3540b687c3de7a944d49da419e52cdbb
Andrew Hu <andrew.n.hu.87@gmail.com>
2021-02-25 21:25:18 -0500

    simplified even more

--- xq_app/src/utils/pagination.ts
+++ xq_app/src/utils/pagination.ts
@@ -4,20 +4,21 @@ export default function (current: number, last: number) {
   const delta = 2;
   const [left, right] = [current - delta, current + delta + 1];
 
-  const numbered = range(1, last).filter(
-    (i) => i === 1 || i === last || (i >= left && i < right),
+  const numbered = range(2, last).filter(
+    (i) => i === last || (i >= left && i < right),
   );
 
   let l = 1;
-  return numbered.reduce<(string | number)[]>((acc, i) => {
-    if (i !== 1) {
+  return numbered.reduce<(string | number)[]>(
+    (acc, i) => {
       if (i - l === 2) {
         acc = [...acc, l + 1];
       } else if (i - l !== 1) {
         acc = [...acc, '...'];
       }
-    }
-    l = i;
-    return [...acc, i];
-  }, []);
+      l = i;
+      return [...acc, i];
+    },
+    [1],
+  );
 }
