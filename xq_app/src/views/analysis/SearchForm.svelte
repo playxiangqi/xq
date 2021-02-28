40d4ec7412cf91aadfacc486bcc821872618b189
Andrew Hu <andrew.n.hu.87@gmail.com>
2021-02-28 00:02:21 -0500

    update: 💄  Improve search form flex container wrap/spacing

--- xq_app/src/views/analysis/SearchForm.svelte
+++ xq_app/src/views/analysis/SearchForm.svelte
@@ -43,8 +43,8 @@
   }
 </script>
 
-<div class="search-form p-3">
-  <div class="field is-grouped">
+<div class="search-form p-2">
+  <div class="search-form-field field is-grouped">
     <div class="control red-player-input">
       <label for="red-player-input" class="label">Player</label>
       <input
@@ -91,43 +91,45 @@
         </select>
       </div>
     </div>
-    <div class="control move-count-min-input">
-      <label for="move-count-min-input" class="label">Moves</label>
-      <input
-        class="input numeric-input"
-        type="number"
-        placeholder="Min"
-        min="0"
-        max="500"
-        bind:value={minMoves}
-      />
-    </div>
-    <div class="control move-count-max-input">
-      <label for="move-count-max-input" class="label is-invisible">
-        Max Moves
-      </label>
-      <input
-        class="input numeric-input"
-        type="number"
-        placeholder="Max"
-        min="0"
-        max="500"
-        bind:value={maxMoves}
-      />
-    </div>
-    <div class="control limit-dropdown">
-      <label for="limit-input" class="label">Limit</label>
-      <div class="select">
-        <select bind:value={limit}>
-          {#each [10, 25, 50, 100] as o}
-            <option>{o}</option>
-          {/each}
-        </select>
+    <div class="move-count-field field is-grouped">
+      <div class="control move-count-min-input">
+        <label for="move-count-min-input" class="label">Moves</label>
+        <input
+          class="input numeric-input"
+          type="number"
+          placeholder="Min"
+          min="0"
+          max="500"
+          bind:value={minMoves}
+        />
       </div>
-    </div>
-    <div class="control ml-3">
-      <label for="search-button" class="label is-invisible">Search</label>
-      <button class="button is-link" on:click={searchGames}>Search</button>
+      <div class="control move-count-max-input">
+        <label for="move-count-max-input" class="label is-invisible">
+          Max Moves
+        </label>
+        <input
+          class="input numeric-input"
+          type="number"
+          placeholder="Max"
+          min="0"
+          max="500"
+          bind:value={maxMoves}
+        />
+      </div>
+      <div class="control limit-dropdown">
+        <label for="limit-input" class="label">Limit</label>
+        <div class="select">
+          <select bind:value={limit}>
+            {#each [10, 25, 50, 100] as o}
+              <option>{o}</option>
+            {/each}
+          </select>
+        </div>
+      </div>
+      <div class="control ml-3">
+        <label for="search-button" class="label is-invisible">Search</label>
+        <button class="button is-link" on:click={searchGames}>Search</button>
+      </div>
     </div>
   </div>
 </div>
@@ -135,6 +137,17 @@
 <style lang="scss">
   .search-form {
     height: 100%;
+
+    .search-form-field {
+      margin-top: -15px;
+
+      flex-direction: row;
+      flex-wrap: wrap;
+
+      & > * {
+        margin-top: 15px;
+      }
+    }
 
     .numeric-input {
       width: 100px;
