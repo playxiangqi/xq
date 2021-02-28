1dc556f6212d2f24159f791eb6cdbd9e0316cc86
Andrew Hu <andrew.n.hu.87@gmail.com>
2021-02-28 16:41:10 -0500

    fixed tests

--- xq_portal/test/xq/parser/axf_test.exs
+++ xq_portal/test/xq/parser/axf_test.exs
@@ -2,6 +2,7 @@ defmodule XQ.Parser.AXFTest do
   use ExUnit.Case
 
   @axis ~w(P p C c R r K k)
+  @fixed ~w(B b N n A a)
   @direction ~w(+ = -)
   @position ~w(+ -)
   @range 0..9
@@ -22,7 +23,29 @@ defmodule XQ.Parser.AXFTest do
 
     assert Map.has_key?(params, :pos)
     assert Map.has_key?(params, :abbrev)
+    assert params[:prev_file] == nil
     assert Map.has_key?(params, :dir)
     assert Map.has_key?(params, :mvmt)
+  end
+
+  test "Parametrize exact fixed notation" do
+    matches = Enum.map([@fixed, @range, @direction, @range], &Enum.random(&1))
+    params = XQ.Parser.AXF.parametrize(matches)
+
+    assert Map.has_key?(params, :abbrev)
+    assert Map.has_key?(params, :prev_file)
+    assert Map.has_key?(params, :dir)
+    assert Map.has_key?(params, :next_file)
+  end
+
+  test "Parametrize positional fixed notation" do
+    matches = Enum.map([@position, @fixed, @direction, @range], &Enum.random(&1))
+    params = XQ.Parser.AXF.parametrize(matches)
+
+    assert Map.has_key?(params, :pos)
+    assert Map.has_key?(params, :abbrev)
+    assert params[:prev_file] == nil
+    assert Map.has_key?(params, :dir)
+    assert Map.has_key?(params, :next_file)
   end
 end
