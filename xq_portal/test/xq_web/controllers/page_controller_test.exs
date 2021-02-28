c2c09266c12ab0b254e050b4c8f46a4d38e23028
Andrew Hu <andrew.n.hu.87@gmail.com>
2021-02-28 02:32:50 -0500

    update: ✅ Add xq_portal tests to CI/CD workflow

--- xq_portal/test/xq_web/controllers/page_controller_test.exs
+++ xq_portal/test/xq_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule XQWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Play XiangQi"
   end
 end
