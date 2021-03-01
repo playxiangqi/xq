e38e3e0cad3dd3a705f3545d5faba08f97378e5a
Andrew Hu <andrew.hu@pithecine.com>
2021-03-01 00:37:11 -0500

    update: 🤡💄  Mock game review component layout w/ new analysis panel (#66)

---
+++ xq_app/src/views/analysis/EngineAnalysisPanel.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+</script>
+
+<div class="panel engine-analysis-panel">
+  <div class="panel-heading">Engine Analysis</div>
+  <div class="panel-tabs">
+    <a>Evaluation</a>
+    <a>Engine Selection</a>
+    <a>Board Setup</a>
+  </div>
+  <div class="content-container" />
+</div>
+
+<style lang="scss">
+  .engine-analysis-panel {
+    margin-left: 50px;
+
+    .content-container {
+      min-height: 700px;
+      height: 700px;
+    }
+  }
+</style>
