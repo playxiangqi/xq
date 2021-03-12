90fa5441f62597e6da14d02b1acb0baf9ca317dc
Andrew Hu <andrew.hu@pithecine.com>
2021-03-12 11:41:12 -0500

    cleanup: 🎨  Code org surrounding analysis props (#119)

---
+++ xq_app/src/views/analysis/store.ts
@@ -0,0 +1,34 @@
+import { writable } from 'svelte/store';
+import { createChannel, PhoenixPayload } from 'utils/channels';
+import type { EngineResults } from './types';
+
+export function createAnalysisStore(username: string) {
+  const store = writable<EngineResults>({
+    best_move: [],
+    results: [],
+  });
+
+  function loadEngineResults(payload: PhoenixPayload) {
+    // console.log('engineResults: ', payload);
+    store.update(() => payload as EngineResults);
+  }
+
+  function dispatcher(event: string, payload: PhoenixPayload) {
+    const dispatch: {
+      [event: string]: (payload: PhoenixPayload) => void;
+    } = {
+      [`analysis:${username}`]: loadEngineResults,
+    };
+
+    return dispatch[event]?.(payload);
+  }
+
+  const { broadcast } = createChannel(`analysis:${username}`, dispatcher);
+  const pushAnalysis = (payload: PhoenixPayload) =>
+    broadcast('analysis:board_state', payload);
+
+  return {
+    store,
+    pushAnalysis,
+  };
+}
